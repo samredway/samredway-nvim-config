9c34355fddf5f20b5cb171460e561b703373ec92
Sam Redway <sam.redway@outlook.com>
2024-07-19 19:27:01 +0100

    add support for terraform

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -167,21 +167,14 @@ return {
             },
           },
         },
-
+        terraformls = {},
         tsserver = {},
-        --
-
         lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
           settings = {
             Lua = {
               completion = {
                 callSnippet = 'Replace',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
             },
           },
         },
