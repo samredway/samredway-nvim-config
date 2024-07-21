663d7a8dd6388d8aa4d5e837ab505a7e178efac3
Sam Redway <sam.redway@outlook.com>
2024-07-21 17:06:05 +0100

    Use nerd font. Do NOT install mypy with mason as this mypy will not have access to type stubs! Let it default to the terminal one which will be correct as long as virtualenv is active

--- lua/custom/plugins/guard.lua
+++ lua/custom/plugins/guard.lua
@@ -1,35 +1,36 @@
 return {
   'nvimdev/guard.nvim',
-  -- Builtin configuration, optional
   dependencies = {
     'nvimdev/guard-collection',
+    'williamboman/mason.nvim',
+    'williamboman/mason-tool-installer.nvim',
   },
   config = function()
     local ft = require 'guard.filetype'
 
-    -- Assuming you have guard-collection
-    ft('python'):fmt('black'):lint 'mypy'
+    -- Ensure Mason is correctly set up with the tools we need
     require('mason').setup()
 
+    -- Define the formatters and linters
+    ft('python'):fmt('black'):lint 'mypy'
     ft('typescript,javascript,typescriptreact'):fmt 'prettier'
 
-    -- You can add other tools here that you want Mason to install
-    -- for you, so that they are available from within Neovim.
+    -- Ensure Mason installs the required tools
     local ensure_installed = {
       'black',
-      'mypy',
       'prettier',
     }
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+    require('mason-tool-installer').setup {
+      ensure_installed = ensure_installed,
+    }
 
-    -- Call setup() LAST!
+    -- Call Guard setup LAST!
     require('guard').setup {
       -- Choose to format on every write to a buffer
       fmt_on_save = true,
       -- Use lsp if no formatter was defined for this filetype
       lsp_as_default_formatter = false,
-      -- By default, Guard writes the buffer on every format
-      -- You can disable this by setting:
+      -- You can disable auto save on format if desired
       -- save_on_fmt = false,
     }
   end,
