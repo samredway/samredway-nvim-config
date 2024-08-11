06fa1c98bd933413acaeb2fbcfb46086ec8c7d1e
Sam Redway <sam.redway@outlook.com>
2024-08-11 18:02:05 +0100

    add markdown preview

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -25,6 +25,7 @@ require('lazy').setup({
   require '/plugins/mini',
   require '/plugins/treesitter',
   require '/plugins/guard',
+  require '/plugins/markdown-preview',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
