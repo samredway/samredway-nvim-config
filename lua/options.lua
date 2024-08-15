ffcd6348ebef9ac5d80ee401c5db632066a00e92
Sam Redway <sam.redway@outlook.com>
2024-08-15 19:06:35 +0100

    allow mouse

--- lua/options.lua
+++ lua/options.lua
@@ -6,7 +6,7 @@
 vim.opt.number = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = ''
+vim.opt.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
