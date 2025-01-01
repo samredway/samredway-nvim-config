ccfe890f10e9d0d76e5ed59a7d46313ff4b17e4c
Sam Redway <sam.redway@outlook.com>
2025-01-01 11:57:43 +0000

    remove stylua from ensure installed for lsp config as not allowed

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -167,9 +167,7 @@ return {
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-      })
+      vim.list_extend(ensure_installed, {})
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       -- Mason setup for LSP configuration
