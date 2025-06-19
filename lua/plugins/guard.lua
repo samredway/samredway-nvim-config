a07a86c55b847e722d444a3433fae5563dc44376
Sam Redway <sam.redway@polimonitor.com>
2025-06-19 15:37:46 +0100

    use pyright lsp and rely on guard for all linting and formatting

--- lua/plugins/guard.lua
+++ lua/plugins/guard.lua
@@ -1,38 +1,38 @@
 return {
   'nvimdev/guard.nvim',
-    dependencies = {
-        "nvimdev/guard-collection",
-    },
-    config = function()
+  dependencies = {
+    'nvimdev/guard-collection',
+  },
+  config = function()
     local ft = require 'guard.filetype'
 
-    -- Define the formatters and linters
     ft('typescript,javascript,typescriptreact'):fmt 'prettier'
-    ft('python'):fmt('black'):lint 'mypy'
+    ft('python')
+      :fmt('black')
+      :fmt('isort')
+      :lint('flake8')
+      :lint('mypy')
     ft('go'):fmt('gofmt')
     ft('cpp'):fmt('clang-format')
 
-    -- Ensure Mason installs the required tools
     local ensure_installed = {
       'black',
+      'isort',
+      'flake8',
+      'mypy',
       'prettier',
     }
+
     require('mason-tool-installer').setup {
       ensure_installed = ensure_installed,
     }
 
-   -- change this anywhere in your config (or not), these are the defaults
-  vim.g.guard_config = {
-      -- format on write to buffer
+    vim.g.guard_config = {
       fmt_on_save = true,
-      -- use lsp if no formatter was defined for this filetype
       lsp_as_default_formatter = false,
-      -- whether or not to save the buffer after formatting
       save_on_fmt = true,
-      -- automatic linting
       auto_lint = true,
-      -- how frequently can linters be called
-      lint_interval = 500
-  }
+      lint_interval = 500,
+    }
   end,
 }
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -119,17 +119,13 @@ return {
 
       local servers = {
         gopls = {},
-        pylsp = {
+        pyright = {
           settings = {
-            pylsp = {
-              plugins = {
-                flake8 = { enabled = true },
-                pycodestyle = { enabled = false },
-                pyflakes = { enabled = false },
-                pylint = { enabled = false },
-                mccabe = { enabled = false },
-                autopep8 = { enabled = false },
-                yapf = { enabled = false },
+            python = {
+              analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                typeCheckingMode = "off", -- ← disables diagnostics I use nvim.guard
               },
             },
           },
