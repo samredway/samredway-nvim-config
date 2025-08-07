d069bef734799d3d9aa4dedee14eaa046129e41a
Sam Redway <sam.redway@outlook.com>
2025-08-07 21:26:16 +0100

    remove unused plugin

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -26,9 +26,7 @@ require('lazy').setup({
   require '/plugins/treesitter',
   require '/plugins/guard',
   require '/plugins/markdown-preview',
-  -- some plugins I have config for but dont really use
   require '/plugins/nvim-tree',
-  -- require '/plugins/avante-ai',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
