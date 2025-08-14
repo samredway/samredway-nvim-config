0a531f9300dfec9cd7a2c89233a3c091966f90a5
Sam Redway <sam.redway@outlook.com>
2025-08-14 20:58:49 +0100

    tidy up ftplugin folder

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -3,7 +3,3 @@ vim.bo.shiftwidth = 2
 vim.bo.expandtab = true
 vim.bo.softtabstop = 2
 
--- Set path to libs for java lsp
-vim.env.JAVA_TOOL_OPTIONS = "-javaagent:" .. vim.fn.expand("~/.local/share/nvim/mason/packages/lombok-nightly/lombok.jar")
-
-
