02e95646668457848dd9e3ae016a28c6a92e3e84
Sam Redway <sam.redway@outlook.com>
2025-08-14 23:42:50 +0100

    Fix removed config

--- lua/plugins/guard.lua
+++ lua/plugins/guard.lua
@@ -14,5 +14,13 @@ return {
       :lint('mypy')
     ft('go'):fmt('gofmt')
     ft('cpp'):fmt('clang-format')
+
+    vim.g.guard_config = {
+      fmt_on_save = true,
+      lsp_as_default_formatter = false,
+      save_on_fmt = true,
+      auto_lint = true,
+      lint_interval = 500,
+    }
   end,
 }
