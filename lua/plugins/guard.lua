12729fba82a6a898d73a6a1bca4adc2b04d01ce2
Sam Redway <sam.redway@polimonitor.com>
2025-08-15 12:43:34 +0100

    Final fixes to python tooling. Dont use MAson!

--- lua/plugins/guard.lua
+++ lua/plugins/guard.lua
@@ -11,17 +11,12 @@ return {
     ft('typescript,javascript,typescriptreact'):fmt 'prettier'
     ft('python')
       :fmt('isort')
-      -- Close but breaks if the file has an error in it and cant be imported
-      -- In fact this will always happen as blck needs to import the file to 
-      -- run. Maybe we live with the ugly error coming up each time?
-      :append({
-        cmd = 'black',
-        args = { '--quiet', '-' },
-        stdin = true,
-      })
-      -- Never install mypy to mason or this will use mason instead of your venv
-      -- always activate venv before running nvim or you will use sys instead of
-      -- venv
+      -- Never install black or mypy with mason or that will get preference 
+      -- over the version accessibly from your termail. Instead make sure
+      -- you activate your vitual env before you open nvim and that version 
+      -- will be used (otherwise it will be the system version)
+      -- Check the version being used by doing ':! which mypy'
+      :append('black')
       :lint('mypy')
       -- apparently cant chain this with flake8 so that gets done by the lsp
     ft('go'):fmt('gofmt')
